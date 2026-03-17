8af3a6738c3e96bb340144ea14400b7ba649e114
Clove Twilight <clovetwilight3@outlook.com>
2026-03-17 09:17:48 +0000

    move port for Doughmination Web

--- co/system/backend/main.py
+++ co/system/backend/main.py
@@ -55,4 +55,4 @@ app.include_router(static.router, tags=["Static Files"])  # Must be last for cat
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
