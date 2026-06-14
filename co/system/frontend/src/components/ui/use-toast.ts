dc3653846ee2d3c02b6dc3f58946f5b6322c32e0
Clove Twilight <admin@doughmination.win>
2026-06-14 19:21:05 +0100

    Fixes to system site

--- co/system/frontend/src/components/ui/use-toast.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2026 Clove Twilight
- * Licensed under the ESAL-1.3 Licence.
- * See LICENCE.md in the project root for full licence information.
- */
-
-import { useToast, toast } from "@/hooks/use-toast";
-
-export { useToast, toast };
