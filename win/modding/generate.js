ba5e7d3883cdbfb351895fa07b720ed824ec1597
Clove Twilight <admin@doughmination.win>
2026-06-14 18:38:44 +0100

    move site about

--- win/modding/generate.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const dir = './assets/crafts';
-const files = fs.readdirSync(dir).filter(f => /\.(png|jpe?g|gif|webp)$/i.test(f));
-fs.writeFileSync(path.join(dir, 'manifest.json'), JSON.stringify(files, null, 2));
-console.log(`Manifest written with ${files.length} file(s).`);
