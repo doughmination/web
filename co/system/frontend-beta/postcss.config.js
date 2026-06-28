60e647e6ca8dfbb2fe05aac54bb7009292103353
Clove Twilight <admin@doughmination.win>
2026-06-28 05:12:52 +0100

    AAAAAAAAAAAAAAAAAAAA

---
+++ co/system/frontend-beta/postcss.config.js
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2026 Clove Twilight
+ * Licensed under the ESAL-2.0 Licence.
+ * See LICENCE.md in the project root for full licence information.
+ */
+
+export default {
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
+};
---
+++ co/system/frontend-beta/src/main.tsx
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2026 Clove Twilight
+ * Licensed under the ESAL-1.3 Licence.
+ * See LICENCE.md in the project root for full licence information.
+ */
+
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./css/main.css";
+
+createRoot(document.getElementById("root")!).render(<App />);
