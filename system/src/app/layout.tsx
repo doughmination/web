f400f18e6a4e0053df01a99cf36cc2cdf9913be7
Clove Twilight <clovetwilight3@outlook.com>
2026-07-23 11:12:26 +0100

    fix icon

--- system/src/app/layout.tsx
+++ system/src/app/layout.tsx
@@ -38,8 +38,8 @@ export const metadata: Metadata = {
     images: [
       {
         url: "https://c.stupid.cat/assets/favicon/avatar.png",
-        width: 1200,
-        height: 630,
+        width: 800,
+        height: 800,
       },
     ],
     type: "website",
